389da4d57c25d24a1f5bc7ade9988184e0fa227a
strange-tiger <jinolee9525@gmail.com>
2023-01-05 07:14:00 +0900

    [Feat] 게임매니저 & 게임종료 UI 구현
    - 게임 매니저 : 게임 종료와 UI, 플레이어 스폰을 다룬다.
    - 게임 종료 UI : ESC키로 활성화, 재시작과 게임 종료가 가능

---
+++ Assignment_MapTool_Donggas/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject _ui;
+    [SerializeField] GameObject _player;
+    [SerializeField] Button _restartBtn;
+    [SerializeField] Button _quitBtn;
+
+    private static readonly Vector3 INIT_PLAYER_POS = new Vector3(0f, 0.5f, 0f);
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _restartBtn.onClick.AddListener(Restart);
+        _quitBtn.onClick.AddListener(Quit);
+
+        _ui.SetActive(false);
+        _player.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        _restartBtn.onClick.RemoveListener(Restart);
+        _quitBtn.onClick.RemoveListener(Quit);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            SpawnPlayer();
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleUI();
+        }
+    }
+
+    private void SpawnPlayer()
+    {
+        _player.transform.position = INIT_PLAYER_POS;
+        _player.SetActive(true);
+    }
+
+    private void ToggleUI()
+    {
+        _ui.SetActive(!_ui.activeSelf);
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
